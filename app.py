b08dabf37d9e624c1fa86c6ee3d7177badfa64b0
Alfredo Santos <thirdsantos666@gmail.com>
2025-04-03 08:59:45 +0800

    another one

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
+
+import eventlet
+eventlet.monkey_patch()
+
 import os
 import json
-import eventlet
-import eventlet.wsgi
 from flask import Flask
 from flask_socketio import SocketIO
 import firebase_admin
 from firebase_admin import credentials, db
 from dotenv import load_dotenv
 
-eventlet.monkey_patch()
 
 load_dotenv()
 
+
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
+
 
 firebase_credentials = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")
 
@@ -29,27 +32,26 @@ else:
     print("Error: GOOGLE_APPLICATION_CREDENTIALS_JSON not found in environment variables.")
     exit()
 
+
 ref = db.reference("Sensors")
+
 
 @socketio.on("connect")
 def connection():
     print("A Client is Connected!")
     socketio.emit("Message", "You're now connected")
 
+
 @socketio.on("disconnect")
 def disconnection():
     print("A Client is Disconnected!")
 
-@socketio.on("message")
-def handle_message(msg):
-    print(f"Received message: {msg}")
-    socketio.send(f"Server received: {msg}")
 
 @socketio.on("sensors")
 def handle_sensors(data):
     try:
         print(f"Received data: {data}")
-        
+
         if isinstance(data, dict):
             if "PH" in data and "Temperature" in data:
                 
@@ -66,6 +68,7 @@ def handle_sensors(data):
     except Exception as e:
         print(f"Error updating Firebase: {e}")
         socketio.emit('sensor_response', 'Failed to update data')
+
 
 if __name__ == "__main__":
     print("WebSocket Server is Running...")

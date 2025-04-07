dc712e730e797bb552dc7d7dc832aaa453c7d6ee
Alfredo Santos <thirdsantos666@gmail.com>
2025-04-07 18:26:04 +0800

    notification added

--- app.py
+++ app.py
@@ -51,19 +51,37 @@ def disconnection():
     print("A Client is Disconnected!")
 
 def updateToDb(data):
-    if isinstance(data, dict):
-        if "PH" in data and "Temperature" in data and "Turbidity" in data:
+    
                 
             ref.update(data)  
             print("Successfully updated Firebase")
-        else:
-                print("Error: Missing 'PH' or 'Temperature' data or 'Turbidity' data")
-                socketio.emit('sensor_response', 'Invalid data format')
-    else:
-        print("Error: Data is not in the correct format")
-        socketio.emit('sensor_response', 'Invalid data format')
+       
 
 
+def checkTreshHold(data):
+    refPh = db.reference("Notification/PH")
+    refTemp = db.reference("Notification/Temperature")
+    refTurb = db.reference("Notification/Turbidity")
+    
+
+    ph_value = refPh.get()
+    temp_value = refTemp.get()
+    turb_value = refTurb.get()
+
+    phValue = data["PH"]
+    temperatureValue = data["Temperature"]
+    turbidityValue = data["Turbidity"]
+
+
+    if ph_value["Min"] > phValue or ph_value["Max"] < phValue:
+        socketio.emit('PHNotif', {"alert": "PH value is out of range!"})
+    
+    if temp_value["Min"] > temperatureValue or temp_value["Max"] < temperatureValue:
+        socketio.emit('TemperatureNotif', {"alert": "Temperature value is out of range!"})
+    
+    if turb_value["Min"] > turbidityValue or turb_value["Max"] < turbidityValue:
+        socketio.emit('TurbidityNotif', {"alert": "Turbidity value is out of range!"})
+        
 
 
 refNotif = db.reference("Notifications")
@@ -71,9 +89,18 @@ refNotif = db.reference("Notifications")
 @socketio.on("sensors")
 def handle_sensors(data):
     try:
-        print(f"Received data: {data}")
+        if isinstance(data, dict):
+            if "PH" in data and "Temperature" in data and "Turbidity" in data:
+                print(f"Received data: {data}")
+                updateToDb(data)
+                checkTreshHold(data)
 
-        updateToDb(data)
+            else:
+                print("Error: Missing 'PH' or 'Temperature' data or 'Turbidity' data")
+                socketio.emit('sensor_response', 'Invalid data format')
+        else:
+            print("Error: Data is not in the correct format")
+            socketio.emit('sensor_response', 'Invalid data format')
         
 
 
